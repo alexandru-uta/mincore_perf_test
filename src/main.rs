0a30a7f8f5a28e606f1ff6a213982bdfe666d9b7
Alex Uta <alexandru.uta@gmail.com>
2025-03-26 15:22:29 +0000

    Add mprotect() calls to check how mincore behaves when the VMA structures are broken up into multiple smaller pieces.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use clap::Parser;
 use libc::{
-    mmap, munmap, sysconf, MAP_ANONYMOUS, MAP_PRIVATE, PROT_READ, PROT_WRITE, _SC_PAGESIZE,
+    mmap, mprotect, munmap, sysconf, MAP_ANONYMOUS, MAP_PRIVATE, PROT_READ, PROT_WRITE,
+    _SC_PAGESIZE,
 };
-use std::ptr;
+use std::{ffi::c_void, ptr};
 
 const GB: usize = 1024 * 1024 * 1024;
 
@@ -15,8 +16,10 @@ const GB: usize = 1024 * 1024 * 1024;
 pub struct MincorePerfArgs {
     #[arg(short, long, value_name = "REGION_SIZE", default_value = "1")]
     pub region_size: Option<i32>,
-    #[arg(short, long, default_value = "25", value_name = "PERCENT_PAGES")]
+    #[arg(short, long, default_value = "50", value_name = "PERCENT_PAGES")]
     pub percentage_pages: Option<i32>,
+    #[arg(short, long, default_value = "1", value_name = "MPROTECT_GRANULARITY")]
+    pub mprotect_granularity: Option<i32>,
 }
 
 fn main() {
@@ -27,6 +30,7 @@ fn main() {
     let args = MincorePerfArgs::parse();
     let region_size = args.region_size.unwrap() as usize * GB;
     let percentage_pages = args.percentage_pages.unwrap();
+    let mprotect_granularity = args.mprotect_granularity.unwrap() as usize;
 
     let num_pages = region_size / page_size;
 
@@ -57,6 +61,29 @@ fn main() {
             // Write to the page to make it dirty.
             *(addr as *mut u8).add(i * page_size) = 131;
         }
+        // Don't mprotect() outside of the region.
+        let mprotect_range = if i + mprotect_granularity < num_pages {
+            mprotect_granularity
+        } else {
+            num_pages - i
+        };
+        let ret = unsafe {
+            mprotect(
+                (addr as *mut u8).add(i * page_size) as *mut c_void,
+                page_size * mprotect_range,
+                PROT_WRITE,
+            )
+        };
+        if ret != 0 {
+            let errno = std::io::Error::last_os_error().raw_os_error().unwrap();
+            eprintln!("Error: {}", errno);
+            if errno == libc::ENOMEM {
+                eprintln!("Most likely the numer of VMAs per process is too low. Try to increase the number of VMAs.");
+                eprintln!("You can increase the number of VMAs by running the following command:");
+                eprintln!("sysctl -w vm.max_map_count=262144");
+            }
+            return;
+        }
     }
 
     // Use mincore to check if pages are present in memory.
@@ -69,20 +96,36 @@ fn main() {
     // Get start time.
     let start = std::time::Instant::now();
     // Run mincore 100 times.
-    for _ in 0..100 {
+    let runs = 100;
+    for _ in 0..runs {
         unsafe {
             // Call mincore for the whole region and gather results.
             let res = libc::mincore(addr, region_size, vec.as_mut_ptr());
             // Count the number of pages that are present in memory.
             if res == 0 {
-                present_count = vec.iter().filter(|&&x| x > 0).count();
+                present_count = vec.iter().filter(|&&x| x == 1).count();
             }
         }
     }
 
-    // Print time taken in nanoseconds.
-    println!("Time taken: {:?}", start.elapsed().as_nanos() / 100);
-    //println!("Present pages: {}", present_count);
+    // println!(
+    //     "One mincore for {}GiB ({percentage_pages}%) takes {:.3}s",
+    //     (region_size as f32 / 1024.0 / 1024.0 / 1024.0 * 1000.0).round() / 1000.0,
+    //     start.elapsed().as_secs_f32() / runs as f32,
+    // );
+    // println!(
+    //     "Present pages: {} num_pages:{num_pages} skip_pages:{skip_pages}",
+    //     present_count
+    // );
+
+    println!("{:.5}", start.elapsed().as_secs_f32() / runs as f32);
+
+    // Print the number of VMAs in the process, read file /proc/pid/maps.
+    // And count the number of VMAs.
+    // let pid = std::process::id();
+    // let maps = std::fs::read_to_string(format!("/proc/{pid}/maps")).unwrap();
+    // let vmas = maps.lines().count();
+    // println!("VMAs: {}", vmas);
 
     // Due to rounding errors, the present count may be off by 1.
     assert!(present_count.abs_diff(num_pages / skip_pages) < 2);
--- src/run_param_sweep.sh
+++ src/run_param_sweep.sh
@@ -3,23 +3,36 @@
 # Script that runs a parameter sweep for the rust program defined in
 # this project. The script returns a csv file with the results of the
 # following type:
-# region_size, percentage_pages, time
+# region_size, percentage_pages, mprotect_granularity, time
 
 # The output of the rust program is something like:
-# Time taken: 88.30505ms.
+# 0.04434
+# the time is in seconds.
+
+# The script receives one argument, which is the output file where the
+# results will be stored.
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <output_file>"
+    exit 1
+fi
 
 cargo build --release
 
-REGION_SIZES=(1 2 4 8 10 12 16 20 24 30)
-PERCENTAGES=(1 5 10 20 25 50 75 80 90 100)
+REGION_SIZES=(1 2 4 8 10 12 16)
+PERCENTAGES=(1 5 10 20 25 50)
+MPROTECT_GRANULARITY=(1 2 4 8 16 32 64 128)
 
-echo "region_size,percentage_pages,time" > results.csv
+OUTPUT_FILE=$1
+
+echo "region_size,percentage_pages,mprotect_granularity,time" > $OUTPUT_FILE
 
 for region_size in ${REGION_SIZES[@]}; do
     for percentage in ${PERCENTAGES[@]}; do
-        echo "Running with region_size: $region_size, percentage: $percentage"
-        output_result=`./target/release/pagemap_perf_test --region-size=${region_size} --percentage-pages=${percentage}`
-        result_time=$(echo $output_result | grep -oP 'Time taken: \K[0-9.]+')
-        echo "$region_size,$percentage,$result_time" >> results.csv
+        for mprotect_granularity in ${MPROTECT_GRANULARITY[@]}; do
+            echo "Running with region_size: $region_size, percentage: $percentage, mprotect_granularity: $mprotect_granularity"
+            runtime=`./target/release/mincore_perf_test --region-size=${region_size} --percentage-pages=${percentage} --mprotect-granularity=${mprotect_granularity}`
+            
+            echo "$region_size,$percentage,$mprotect_granularity,$runtime" >> $OUTPUT_FILE
+        done
     done
 done
